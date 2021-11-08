1e88839c1b3747c50c3cdfde7dc69b94bb34b960
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-11-08 09:45:35 +0900

    create dataset and train

--- example/RelativeScale/create_dataset.py
+++ example/RelativeScale/create_dataset.py
@@ -26,7 +26,7 @@ while cap.isOpened():
 
         ret, img = cap.read()
 
-        # img = cv2.flip(img, 1)
+        img = cv2.flip(img, 1)
 
         cv2.putText(img, f'Waiting for collecting {action.upper()} action...', org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
         cv2.imshow('img', img)
@@ -44,9 +44,9 @@ while cap.isOpened():
 
             if result.multi_hand_landmarks is not None:
                 for res in result.multi_hand_landmarks:
-                    joint = np.zeros((21, 2))
+                    joint = np.zeros((21, 2)) # 넘파이 배열 크기 변경
                     for j, lm in enumerate(res.landmark):
-                        joint[j] = [lm.x, lm.y]
+                        joint[j] = [lm.x, lm.y] # z축 제거, visibility 제거
 
                     # Compute angles between joints
                     v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
@@ -60,7 +60,7 @@ while cap.isOpened():
                         v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
                         v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
 
-                    angle = np.degrees(angle) # Convert radian to degree
+                    angle = np.round(np.degrees(angle) / 360, 6) # Convert radian to degree
 
                     angle_label = np.array([angle], dtype=np.float32)
                     angle_label = np.append(angle_label, idx)
@@ -70,6 +70,8 @@ while cap.isOpened():
                     data.append(d)
 
                     mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+
+            img = cv2.flip(img, 1)
 
             cv2.imshow('img', img)
             if cv2.waitKey(1) == ord('q'):
@@ -86,5 +88,6 @@ while cap.isOpened():
 
         full_seq_data = np.array(full_seq_data)
         # print(action, full_seq_data.shape)
-        # np.save(os.path.join('dataset', f'seq_{action}_{created_time}'), full_seq_data)
+        # 데이터 저장
+        np.save(os.path.join('dataset', f'seq_{action}_{created_time}'), full_seq_data)
     break
